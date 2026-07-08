46492774bf2495766d89f5476a379873d2a0a029
Blapper <108067067+Blapper@users.noreply.github.com>
2026-07-08 15:32:00 +0200

    Spring initializr eingerichtet

---
+++ src/main/java/assinment/dhbw/olaf/rezeptanpassung/RezeptanpassungApplication.java
@@ -0,0 +1,13 @@
+package assinment.dhbw.olaf.rezeptanpassung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezeptanpassungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezeptanpassungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/assinment/dhbw/olaf/rezeptanpassung/RezeptanpassungApplicationTests.java
@@ -0,0 +1,13 @@
+package assinment.dhbw.olaf.rezeptanpassung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezeptanpassungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
